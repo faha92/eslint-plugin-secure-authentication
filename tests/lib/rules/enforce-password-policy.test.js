512ed141ef73dda91b65aa76b95363c6d1fa7f66
faha92 <91741100+faha92@users.noreply.github.com>
2023-05-30 19:06:14 +0200

    Update enforce-password-policy.test.js

--- tests/lib/rules/enforce-password-policy.test.js
+++ tests/lib/rules/enforce-password-policy.test.js
@@ -1,102 +1,52 @@
-module.exports = {
-  meta: {
-    type: "problem",
-    docs: {
-      description:
-        "Ensure strong password policy is enforced for user registration",
-      category: "Security",
-      recommended: true,
-    },
-    schema: [],
-    fixable: "code",
-  },
+const { RuleTester } = require('eslint');
+const rule = require('../../../lib/rules/enforce-password-policy'); // replace with your actual rule file
 
-  create(context) {
-    let hasPasswordValidatorImport = false;
-    let registerRouteNode = null;
-    let passwordLengthCheckNode = null;
-
-    return {
-      ImportDeclaration(node) {
-        if (node.source.value === "password-validator") {
-          hasPasswordValidatorImport = true;
-        }
-      },
-
-      VariableDeclarator(node) {
-        if (
-          node.init &&
-          node.init.callee &&
-          node.init.callee.name === "require" &&
-          node.init.arguments[0].value === "password-validator"
-        ) {
-          hasPasswordValidatorImport = true;
-        }
-      },
-
-      CallExpression(node) {
-        if (
-          node.callee.type === "MemberExpression" &&
-          node.callee.object.name === "router" &&
-          node.callee.property.name === "post" &&
-          node.arguments.length > 0 &&
-          node.arguments[0].value === "/register"
-        ) {
-          registerRouteNode = node;
-
-          if (
-            node.arguments.some(
-              (arg) =>
-                arg.type === "Identifier" && arg.name === "passwordValidator"
-            )
-          ) {
-            hasPasswordValidatorImport = true;
+const ruleTester = new RuleTester({
+  parserOptions: { ecmaVersion: 2015, sourceType: "module" },
+});
+ruleTester.run('enforce-password-policy', rule, {
+  valid: [
+    // Valid case: both password-validator and register route are present with correct password length check
+    {
+      code: `
+        const passwordValidator = require('password-validator');
+        const router = require('express').Router();
+        
+        const schema = new passwordValidator();
+        schema.is().min(8).has().uppercase().has().lowercase().has().symbols().has().digits().not().spaces();
+        
+        router.post('/register', (req, res) => {
+          const { password } = req.body;
+          if (password.length < 8) {
+            return res.status(400).send('Password must be at least 8 characters');
           }
-        }
-      },
+          if (!schema.validate(password)) {
+            return res.status(400).send('Password is not strong enough');
+          }
+          // Registration logic here...
+        });
+      `,
+    },
+  ],
+  invalid: [
+    // Invalid case: missing password-validator and incorrect password length check
+    {
+      code: `
+        const router = require('express').Router();
 
-      IfStatement(node) {
-        if (
-          node.test.left &&
-          node.test.left.property &&
-          node.test.left.property.name === "length" &&
-          node.test.right.value === 8
-        ) {
-          passwordLengthCheckNode = node;
-        }
-      },
-
-      "Program:exit": function () {
-        if (!hasPasswordValidatorImport && registerRouteNode) {
-          context.report({
-            node: registerRouteNode,
-            message: "Use password-validator to enforce strong password policy",
-            fix(fixer) {
-              const importText =
-                "const passwordValidator = require('password-validator');\n\n";
-              const schemaText =
-                "const schema = new passwordValidator();\n\nschema\n  .is().min(8)\n  .has().uppercase()\n  .has().lowercase()\n  .has().symbols()\n  .has().digits()\n  .not().spaces();\n\n";
-
-              const insertImportFix = fixer.insertTextBefore(
-                context.getSourceCode().ast.body[0],
-                importText + schemaText
-              );
-
-              // Insert password validation code after the password length check
-              const insertValidationFix = fixer.insertTextAfter(
-                passwordLengthCheckNode,
-                `
-    if (!schema.validate(password)) {
-      errors.push({ msg: "Password must be at least 8 characters long and include uppercase and lowercase letters, as well as at least one special character" });
-    }
-                `
-              );
-
-              return [insertImportFix, insertValidationFix];
-            },
-          });
-        }
-      },
-    };
-  },
-};
+        router.post('/register', (req, res) => {
+          const { password } = req.body;
+          if (password.length < 6) {
+            return res.status(400).send('Password must be at least 6 characters');
+          }
+          // Registration logic here...
+        });
+      `,
+      errors: [
+        {
+          message: "Use password-validator to enforce strong password policy",
+        },
+      ],
+    },
+  ],
+});
