67589629f9142c40e4de0b74576523323e6b0aea
faha <faha@itu.dk>
2023-05-30 12:01:59 +0200

    updated checkpassword and test

--- tests/lib/rules/check-password-hashing.test.js
+++ tests/lib/rules/check-password-hashing.test.js
@@ -1,11 +1,13 @@
+// Importing necessary libraries
 const RuleTester = require("eslint").RuleTester;
 const rule = require("../../../lib/rules/check-password-hashing");
 
-
+// Instantiating the RuleTester
 const ruleTester = new RuleTester({
-  parserOptions: { ecmaVersion: 2022, sourceType: "module" } // Adjust this according to your needs
+  parserOptions: { ecmaVersion: 2015, sourceType: "module" },
 });
-
+// Running the tests
+// Running the tests
 ruleTester.run("check-password-hashing", rule, {
   valid: [
     {
@@ -13,35 +15,76 @@ ruleTester.run("check-password-hashing", rule, {
         const bcrypt = require("bcryptjs");
         const password = "plain-text-password";
         const hashedPassword = bcrypt.hashSync(password, 10);
-        const user = { username: "user", password: hashedPassword };
-      `
+        const user = new User({ username: "user", password: hashedPassword });
+        user.save();
+      `,
     },
     {
       code: `
-        const user = { username: "user", email: "user@example.com" };
-      `
-    }
+      const bcrypt = require('bcryptjs');
+      const password = 'my-password';
+      const salt = bcrypt.genSaltSync(10);
+      const safePassword = bcrypt.hashSync(password, salt);
+      const user = new User({ username: 'user', password: safePassword });
+      user.save();
+      `,
+    },
   ],
 
   invalid: [
     {
       code: `
         const password = "plain-text-password";
-        const user = { username: "user", password: password };
+        const user = new User({ username: "user", password });
+        user.save();
       `,
       errors: [
         {
-          message: "Do not use plain text passwords. Use hashed passwords instead."
-        }
+          message:
+            "Do not use plain text passwords. Use hashed passwords instead.",
+        },
       ],
-      output: `
-        // importing bcrypt 
-        const bcrypt = require("bcryptjs");
-        const password = "plain-text-password";
-        //  Hashing the password using bcrypt with a salt 
-        const hashedPassword = bcrypt.hashSync(password, 10);
-        const user = { username: "user", password: hashedPassword };
-      `
-    }
-  ]
+    },
+    {
+      code: `
+        const user = new User({ username, password: "password" });
+        user.save();
+      `,
+      errors: [
+        {
+          message:
+            "Do not use plain text passwords. Use hashed passwords instead.",
+        },
+      ],
+    },
+    {
+      code: `
+        const password = "password";
+        const user = new User({ username: "user", password });
+        user.save();
+      `,
+      errors: [
+        {
+          message:
+            "Do not use plain text passwords. Use hashed passwords instead.",
+        },
+      ],
+    },
+    {
+      code: `
+        const bcrypt = require('bcryptjs');
+        const password = 'my-password';
+        const salt = bcrypt.genSaltSync(10);
+        const hashedPassword = bcrypt.hashSync(password, salt);
+        const user = new User({ username: 'user', password: password });
+        user.save();
+      `,
+      errors: [
+        {
+          message:
+            "Do not use plain text passwords. Use hashed passwords instead.",
+        },
+      ],
+    },
+  ],
 });
