3607cf939355fdfe9e06f8f78c64b5363110afa5
faha92 <91741100+faha92@users.noreply.github.com>
2023-06-07 23:15:28 +0200

    Create prevent-brute-force.test.js

---
+++ tests/lib/rules/prevent-brute-force.test.js
@@ -0,0 +1,51 @@
+const rule = require('path_to_your_rule_file');
+const RuleTester = require('eslint').RuleTester;
+
+const ruleTester = new RuleTester({ 
+  parserOptions: { ecmaVersion: 2015, sourceType: 'module' } 
+});
+
+ruleTester.run('prevent-brute-force', rule, {
+  valid: [
+    {
+      code: `
+        const ExpressBrute = require('express-brute');
+        const store = new ExpressBrute.MemoryStore();
+        const bruteforce = new ExpressBrute(store);
+        const router = require('express').Router();
+        router.post('/login', bruteforce.prevent, (req, res) => { /* handle request */ });
+      `,
+    },
+  ],
+
+  invalid: [
+    {
+      code: `
+        const router = require('express').Router();
+        router.post('/login', (req, res) => { /* handle request */ });
+      `,
+      errors: [{
+        message: 'Use express-brute middleware for login brute-force protection',
+      }],
+      output: `
+        const ExpressBrute = require('express-brute');
+        const store = new ExpressBrute.MemoryStore();
+        const bruteforce = new ExpressBrute(store, {
+          freeRetries: 5,
+          minWait: 600 * 1000,
+          failCallback: function (req, res, next, nextValidRequestDate) {
+            console.log('Too many failed login attempts. Rate limiting request.');
+            req.flash(
+              'error',
+              'Too many failed login attempts. Please try again later.'
+            );
+            res.redirect('/users/login');
+          },
+        });
+
+        const router = require('express').Router();
+        router.post('/login', bruteforce.prevent, (req, res) => { /* handle request */ });
+      `,
+    },
+  ],
+});
